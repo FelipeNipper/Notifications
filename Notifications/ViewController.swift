b4c976e676c9a80d83c69f165837da81d7ea93a4
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2022-03-15 13:38:38 -0300

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Felipe Grosze Nipper de Oliveira on 15/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
